826d6d4a67faa3ab8ea9382ae3e94d756a94d1e1
reszka303 <reszka303@op.pl>
2020-03-23 03:12:00 +0100

    12th commit

---
+++ src/main/java/javaStart/task7_arrayOneDimensional/exercise1_mySolution/Application.java
@@ -0,0 +1,27 @@
+package javaStart.task7_arrayOneDimensional.exercise1_mySolution;
+
+import java.util.Random;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayService arrayService1 = new ArrayService();
+        Random random = new Random();
+        int[] numbers1 = new int[random.nextInt(10) + 1];
+        int[] numbers2 = new int[random.nextInt(5) + 1];
+
+        arrayService1.fullingArray(numbers1);
+        arrayService1.calculatingArraySum(numbers1);
+        arrayService1.infoArraySum(numbers1);
+
+        System.out.println();
+        arrayService1.fullingArray(numbers2);
+        arrayService1.calculatingArraySum(numbers2);
+        arrayService1.infoArraySum(numbers2);
+
+        System.out.println();
+        arrayService1.calculatingTwoArraySum(numbers1, numbers2);
+        arrayService1.infoTwoArraySum(numbers1, numbers2);
+
+
+    }
+}
---
+++ src/main/java/javaStart/task7_arrayOneDimensional/exercise1_mySolution/ArrayService.java
@@ -0,0 +1,39 @@
+package javaStart.task7_arrayOneDimensional.exercise1_mySolution;
+
+import java.util.Random;
+
+public class ArrayService {
+    public int[] fullingArray(int[] array) {
+        Random random = new Random();
+        int counter = 0;
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = random.nextInt(20) + 1;
+            counter++;
+            System.out.println(counter + ": " + array[i]);
+        }
+        return array;
+    }
+
+    public double calculatingArraySum(int[] array) {
+        double sum = 0;
+        for (int i = 0; i < array.length ; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public double calculatingTwoArraySum(int[] array1, int[] array2) {
+        double sum = calculatingArraySum(array1) + calculatingArraySum(array2);
+        return sum;
+    }
+
+    public void infoArraySum(int[] array) {
+        double infoSum = calculatingArraySum(array);
+        System.out.println("Sum is: " + infoSum);
+    }
+
+    public void infoTwoArraySum(int[] array1, int[] array2) {
+        double infoSum = calculatingTwoArraySum(array1, array2);
+        System.out.println("Sum of two arrays is: " + infoSum);
+    }
+}
